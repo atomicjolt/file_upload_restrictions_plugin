929bba7769a5bea044fbf150386316f52997b16c
Scott Phillips <scott.phillips@atomicjolt.com>
2026-03-12 07:46:51 -0700

    update to check for nil values properly

--- lib/file_upload_restrictions/files_controller_overrides.rb
+++ lib/file_upload_restrictions/files_controller_overrides.rb
@@ -8,7 +8,8 @@ module FileUploadRestrictions
         size_limit = @context&.account && @context&.account.max_file_size[:value]&.to_f
 
         if (file_size_plugin = Canvas::Plugin.find("file_upload_restrictions")) && file_size_plugin.enabled? && (file_size_plugin.settings[:enable_file_types] == "true")
-          if !(Account::Settings.file_type_restrictions.include? File.extname(params[:attachment][:uploaded_data].original_filename).delete_prefix("."))
+          uploaded_data = params[:attachment]&.[](:uploaded_data)
+          if uploaded_data && !(Account::Settings.file_type_restrictions.include? File.extname(uploaded_data.original_filename).delete_prefix("."))
             custom_message = Canvas::Plugin.find("file_upload_restrictions").settings[:file_type_error_message]
             not_allowed = custom_message.nil? ? "File type not allowed." : custom_message
             render status: :unsupported_media_type, json: {
@@ -17,7 +18,7 @@ module FileUploadRestrictions
             }
             return
           end
-          if (size_limit && ((size_limit * 1024 * 1024) <= params[:attachment][:uploaded_data].tempfile.size))
+          if (uploaded_data&.tempfile && size_limit && ((size_limit * 1024 * 1024) <= uploaded_data.tempfile.size))
             render status: :payload_too_large, json: {
               message: "File size is larger than allowed maximum of #{size_limit} MB",
               aj_message: "File size is larger than allowed maximum of #{size_limit} MB"
